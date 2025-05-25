d383585bf90d1bf3a01e6d00cfa2076c0e0b6bde
kacp-rn <kacp.r.nalepa@gmail.com>
2025-05-25 22:55:51 +0200

    25.05.2025

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,11 @@
 fun greeting()
 {
-    var pozdr: string = "hi"
-    println("pozdr")
+    var pozdr: String = "hi"
+    println(pozdr)
 }
 fun main() {
     val name = "Kotlin"
     println("Hello, " + name + "!")
 
-    for (i in 1 until 5)
-    {
-        println("i = $i")
-    }
+    greeting()
 }
